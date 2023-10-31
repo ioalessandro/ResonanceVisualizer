d313262168753a2bb17cc8e11d898d8b9fd1067f
Alessandro <72192937+ioalessandro@users.noreply.github.com>
2023-10-31 14:34:48 +0100

    Update resvis.js

--- sketches/resvis.js
+++ sketches/resvis.js
@@ -1,49 +1,101 @@
 import * as dat from 'dat.gui'
 
-
 const canvasSketch = require('canvas-sketch');
 const { color } = require('canvas-sketch-util');
-
+const canvasHeight = 2000;
+const aspectRatio = 16/9; 
 
 const settings = {
-  dimensions: [ 2048, 2048 ],
+  dimensions: [ canvasHeight, canvasHeight/aspectRatio ],
   animate: false
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
 
-    let waveA = {
+// #region GUI PROPERTIES
+    let finalWaveA = {
       pos: height/2,
       freq: 0.01,
       amp: 600,
       phase: 0,
+    }
+    let finalWaveB = {
+      pos: height/2,
+      freq: 0.01,
+      amp: 600,
+      phase: 1,
+    }
+    let finalSubwaveA = {
+      pos: 0,
+      freq: 0.01,
+      amp: 600,
+      phase: 1,
+    }
+    let finalSubwaveB= {
+      pos: 0,
+      freq: 0.01,
+      amp: 600,
+      phase: 1,
+    }
+
+    let waveA = {
+      pos: height/2,
+      freq: 0.01,
+      amp: 600,
+      phase: 1,
     }
     let waveB = {
       pos: height/2,
       freq: 0.02,
       amp: 600,
-      phase: 0,
+      phase: 1,
     }
 
     let subwaveA = {
-      pos: 0,
-      freq: 0.01,
-      amp: 100,
-      phase: 0,
-    }
-    let subwaveB = {
-      pos: 0,
+      pos: height/2,
       freq: 0.02,
       amp: 100,
-      phase: 0,
+      phase: 1,
+    }
+    let subwaveB = {
+      pos: height/2,
+      freq: 0.02,
+      amp: 100,
+      phase: 1,
     }
 
-    // #region GUI
+    let isHarmonic = {
+      value: true
+    }
+
+    let resonance = {
+      value: 1
+    }
+    let thickness = {
+      value: 1
+    }
+    let density = {
+      value: 1
+    }
+    let xRotation = {
+      value: 1
+    }
+    let yRotation = {
+      value: 1
+    }
+
+
+    // # regionend
+
+ 
+
+    // #region GUI MAIN
 
     let gui = new dat.GUI();
-    const guiA = gui.addFolder('waveA')
-    const guiB = gui.addFolder('waveB')
+    const guiFine = gui.addFolder('Fine Control')
+    const guiA = guiFine.addFolder('waveA')
+    const guiB = guiFine.addFolder('waveB')
     const subguiA = guiA.addFolder('subwaveA')
     const subguiB = guiB.addFolder('subwaveB')
     
@@ -64,9 +116,34 @@ const sketch = () => {
     subguiB.add(subwaveB, 'freq', -5, 5)
     subguiB.add(subwaveB, 'amp', -800, 800)
     subguiB.add(subwaveB, 'phase', -Math.PI, Math.PI)
-    // #endregion
+
+    const guiRes = gui.addFolder('Resonance Control')
+    guiRes.add(isHarmonic, 'value', true).name('Harmonic')
+    guiRes.add(resonance, 'value', -1,2).name('Resonance')
+    guiRes.add(thickness, 'value', -1,2).name('Thickness')
+    guiRes.add(xRotation, 'value', -1,2).name('X Rotation')
+    guiRes.add(yRotation, 'value', -1,2).name('Y Rotation')
+2   // #endregion
+
+       
 
     function animate() {
+
+      // #region PARAMETER CALCULATION
+      finalWaveA.freq = waveA.freq * resonance.value
+      finalWaveA.amp = waveA.amp
+      finalWaveA.phase = waveA.phase * xRotation.value
+      finalSubwaveA.freq = subwaveA.freq  * resonance.value
+      finalSubwaveA.amp = subwaveA.amp * thickness.value
+      finalSubwaveA.phase = subwaveA.phase
+      finalWaveB.freq = waveB.freq  //* resonance.value
+      finalWaveB.amp = waveB.amp
+      finalWaveB.phase = waveB.phase * yRotation.value
+      finalSubwaveB.freq = subwaveB.freq  * resonance.value
+      finalSubwaveB.amp = subwaveB.amp   * thickness.value
+      finalSubwaveB.phase = subwaveB.phase
+      // #endregion
+
       requestAnimationFrame(animate)
       context.clearRect(0, 0, width, height);
       
@@ -75,10 +152,10 @@ const sketch = () => {
       context.beginPath();
       
       for(let i = 0; i < width; i=i+0.1){
-        let waveA_i = waveA.pos + Math.sin(i * waveA.freq + waveA.phase) * waveA.amp;
-        let waveB_i = waveB.pos + Math.sin(i * waveB.freq + waveB.phase) * waveB.amp;
-        let subwaveA_i = subwaveA.pos + Math.sin(i * subwaveA.freq + subwaveA.phase) * subwaveA.amp;
-        let subwaveB_i = subwaveB.pos + Math.sin(i * subwaveB.freq + subwaveB.phase) * subwaveB.amp;
+        let waveA_i = finalWaveA.pos + Math.sin(i * finalWaveA.freq + finalWaveA.phase) * finalWaveA.amp;
+        let waveB_i = finalWaveB.pos + Math.sin(i * finalWaveB.freq + finalWaveB.phase) * finalWaveB.amp;
+        let subwaveA_i = finalSubwaveA.pos + Math.sin(i * finalSubwaveA.freq + finalSubwaveA.phase) * finalSubwaveA.amp;
+        let subwaveB_i = finalSubwaveB.pos + Math.sin(i * finalSubwaveB.freq + finalSubwaveB.phase) * finalSubwaveB.amp;
 
         context.lineTo(waveA_i+subwaveA_i, waveB_i+subwaveB_i)
       }
