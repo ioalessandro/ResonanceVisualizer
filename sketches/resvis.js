34219dac9bbebc72be0a16daa984b989be8d5b19
Alessandro <72192937+ioalessandro@users.noreply.github.com>
2023-10-31 16:27:22 +0100

    Update resvis.js

--- sketches/resvis.js
+++ sketches/resvis.js
@@ -3,7 +3,7 @@ import * as dat from 'dat.gui'
 const canvasSketch = require('canvas-sketch');
 const { color } = require('canvas-sketch-util');
 const canvasHeight = 2000;
-const aspectRatio = 16/9; 
+const aspectRatio = 16/9;
 
 const settings = {
   dimensions: [ canvasHeight, canvasHeight/aspectRatio ],
@@ -15,13 +15,13 @@ const sketch = () => {
 
 // #region GUI PROPERTIES
     let finalWaveA = {
-      pos: height/2,
+      pos: 0,
       freq: 0.01,
       amp: 600,
       phase: 0,
     }
     let finalWaveB = {
-      pos: height/2,
+      pos: 0,
       freq: 0.01,
       amp: 600,
       phase: 1,
@@ -40,27 +40,28 @@ const sketch = () => {
     }
 
     let waveA = {
-      pos: height/2,
+      pos: width/2,
       freq: 0.01,
-      amp: 600,
+      amp: 400,
       phase: 1,
     }
+
     let waveB = {
       pos: height/2,
       freq: 0.02,
-      amp: 600,
+      amp: 400,
       phase: 1,
     }
 
     let subwaveA = {
-      pos: height/2,
-      freq: 0.02,
+      pos: 0,
+      freq: 0.1,
       amp: 100,
       phase: 1,
     }
     let subwaveB = {
-      pos: height/2,
-      freq: 0.02,
+      pos: 0,
+      freq: 0.1,
       amp: 100,
       phase: 1,
     }
@@ -69,14 +70,14 @@ const sketch = () => {
       value: true
     }
 
-    let resonance = {
+    let length = {
       value: 1
     }
     let thickness = {
       value: 1
     }
     let density = {
-      value: 1
+      value: 0
     }
     let xRotation = {
       value: 1
@@ -84,16 +85,38 @@ const sketch = () => {
     let yRotation = {
       value: 1
     }
+    let tubeRotation = {
+      value: 1
+    }
+    let xPosition = {
+      value: 1
+    }
+    let yPosition = {
+      value: 1
+    }
+    let scale = {
+      value: 1
+    }
 
+    let lineColor = {
+      value: "#E75932"
+    }
 
-    // # regionend
+    let bgColor = {
+      value: "#242525"
+    }
+    
+
+    let lineWidth = {
+     value: 4
+    }
+    // #endregion
 
  
 
     // #region GUI MAIN
-
     let gui = new dat.GUI();
-    const guiFine = gui.addFolder('Fine Control')
+    const guiFine = gui.addFolder('Kleinteilig')
     const guiA = guiFine.addFolder('waveA')
     const guiB = guiFine.addFolder('waveB')
     const subguiA = guiA.addFolder('subwaveA')
@@ -117,12 +140,25 @@ const sketch = () => {
     subguiB.add(subwaveB, 'amp', -800, 800)
     subguiB.add(subwaveB, 'phase', -Math.PI, Math.PI)
 
-    const guiRes = gui.addFolder('Resonance Control')
+    const guiRes = gui.addFolder('Resonance')
     guiRes.add(isHarmonic, 'value', true).name('Harmonic')
-    guiRes.add(resonance, 'value', -1,2).name('Resonance')
-    guiRes.add(thickness, 'value', -1,2).name('Thickness')
-    guiRes.add(xRotation, 'value', -1,2).name('X Rotation')
-    guiRes.add(yRotation, 'value', -1,2).name('Y Rotation')
+    guiRes.add(length, 'value', -1,2).name('Length')
+    guiRes.add(thickness, 'value', 0,2).name('Thickness')
+    guiRes.add(density, 'value', 0,32).name('Density')
+
+    guiRes.add(tubeRotation, 'value', -1,2).name('Tube Rotation')
+
+    const guiPos = gui.addFolder('Position & Scale')
+    guiPos.add(xPosition, 'value', -1,2).name('X Position')
+    guiPos.add(yPosition, 'value', -1,2).name('Y Position')
+    guiPos.add(xRotation, 'value', -1,2).name('X Rotation')
+    guiPos.add(yRotation, 'value', -1,2).name('Y Rotation')
+    guiPos.add(scale, 'value', -5,5).name('Scale')
+
+    const guiStyle = gui.addFolder('Style')
+    guiStyle.add(lineWidth, 'value', 1, 200).name('Linewidth')
+    guiStyle.add(lineColor, 'value').name('Line Color')
+    guiStyle.add(bgColor, 'value').name('BG Color')
 2   // #endregion
 
        
@@ -130,24 +166,31 @@ const sketch = () => {
     function animate() {
 
       // #region PARAMETER CALCULATION
-      finalWaveA.freq = waveA.freq * resonance.value
-      finalWaveA.amp = waveA.amp
+      finalWaveA.pos = waveA.pos * xPosition.value
+      finalWaveA.freq = waveA.freq * length.value
+      finalWaveA.amp = waveA.amp * scale.value
       finalWaveA.phase = waveA.phase * xRotation.value
-      finalSubwaveA.freq = subwaveA.freq  * resonance.value
+      finalSubwaveA.freq = subwaveA.freq * density.value
       finalSubwaveA.amp = subwaveA.amp * thickness.value
-      finalSubwaveA.phase = subwaveA.phase
-      finalWaveB.freq = waveB.freq  //* resonance.value
-      finalWaveB.amp = waveB.amp
+      finalSubwaveA.phase = subwaveA.phase *tubeRotation.value
+
+      finalWaveB.pos = waveB.pos * yPosition.value
+      finalWaveB.freq = waveB.freq * length.value
+      finalWaveB.amp = waveB.amp * scale.value
       finalWaveB.phase = waveB.phase * yRotation.value
-      finalSubwaveB.freq = subwaveB.freq  * resonance.value
-      finalSubwaveB.amp = subwaveB.amp   * thickness.value
+      finalSubwaveB.freq = subwaveB.freq * density.value
+      finalSubwaveB.amp = subwaveB.amp * thickness.value
       finalSubwaveB.phase = subwaveB.phase
-      // #endregion
 
       requestAnimationFrame(animate)
       context.clearRect(0, 0, width, height);
       
-      
+      context.fillStyle = bgColor.value;
+      context.fillRect(0, 0, width, height);
+      context.strokeStyle = lineColor.value;
+      context.lineWidth = lineWidth.value;
+
+
       context.moveTo(width/2, height/2)
       context.beginPath();
       
